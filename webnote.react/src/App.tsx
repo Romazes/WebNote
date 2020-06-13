d14eba39f4bbda53ca6783cc1d926ab0acbb7b3f
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-06-13 17:44:58 +0300

    Added NotFound page.

--- webnote.react/src/App.tsx
+++ webnote.react/src/App.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import NotFound from "./components/ErrorPages/NotFound/NotFound";
 import { store } from "./actions/store";
 import { Provider } from "react-redux";
 import Notes from "./components/Notes";
 import { Container } from "@material-ui/core";
+import { Router, Route, Switch } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 function App() {
   return (
     <Provider store={store}>
-      <Container maxWidth="lg">
-        <Notes />
-      </Container>
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact component={Notes} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Router>
     </Provider>
   );
 }
